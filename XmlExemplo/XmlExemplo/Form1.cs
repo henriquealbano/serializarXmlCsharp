481e2c7cd8752603a08bde61b3adf8c86e2fb7f1
henriquealbano <73562335+henriquealbano@users.noreply.github.com>
2022-03-19 14:19:33 -0300

    Commit inicial
    inclusão de código inicial

---
+++ XmlExemplo/XmlExemplo/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XmlExemplo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlSerializer plantsXmlserializer = new XmlSerializer(typeof(CATALOG));
+            TextReader text = (TextReader)new StreamReader("C:\\Lab\\plant_catalog.xml");
+            XmlTextReader xmlTextReader = new XmlTextReader(text);
+            xmlTextReader.Read();
+
+            CATALOG plantsXmlObj = (CATALOG)plantsXmlserializer.Deserialize(xmlTextReader);
+
+
+            CATALOGPLANT[] newPlant = new CATALOGPLANT[2];
+            
+
+            foreach (CATALOGPLANT c in plantsXmlObj.PLANT)
+            {
+                MessageBox.Show(c.BOTANICAL) ;
+                newPlant[0] = new CATALOGPLANT();
+                newPlant[0].COMMON = c.COMMON;
+                newPlant[0].BOTANICAL = c.BOTANICAL;
+                newPlant[0].AVAILABILITY = c.AVAILABILITY;
+                newPlant[0].LIGHT  = c.LIGHT;
+                newPlant[0].PRICE= c.PRICE;
+                newPlant[0].ZONE = c.ZONE;
+                break;
+            }
+            newPlant[1] = new CATALOGPLANT();
+            newPlant[1].BOTANICAL = "teste";
+            newPlant[1].COMMON = "teste";
+            newPlant[1].AVAILABILITY = "099";
+            newPlant[1].LIGHT = "teste";
+            newPlant[1].PRICE = "teste";
+            newPlant[1].ZONE = "teste";
+
+            plantsXmlObj.PLANT = newPlant;
+
+            foreach (CATALOGPLANT c in plantsXmlObj.PLANT)
+            {
+                MessageBox.Show(c.BOTANICAL);
+            }
+
+            //XmlSerializer x = new XmlSerializer(typeof(CATALOG));
+            TextWriter writer = new StreamWriter("C:\\Lab\\plant_catalog_modified.xml");
+            plantsXmlserializer.Serialize(writer, plantsXmlObj);
+
+        }
+    }
+}
---
+++ XmlExemplo/XmlExemplo/plantsCatalogXml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlExemplo
+{
+
+    // OBSERVAÇÃO: o código gerado pode exigir pelo menos .NET Framework 4.5 ou .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class CATALOG
+    {
+
+        private CATALOGPLANT[] pLANTField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("PLANT")]
+        public CATALOGPLANT[] PLANT
+        {
+            get
+            {
+                return this.pLANTField;
+            }
+            set
+            {
+                this.pLANTField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class CATALOGPLANT
+    {
+
+        private string cOMMONField;
+
+        private string bOTANICALField;
+
+        private string zONEField;
+
+        private string lIGHTField;
+
+        private string pRICEField;
+
+        private string aVAILABILITYField;
+
+        /// <remarks/>
+        public string COMMON
+        {
+            get
+            {
+                return this.cOMMONField;
+            }
+            set
+            {
+                this.cOMMONField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string BOTANICAL
+        {
+            get
+            {
+                return this.bOTANICALField;
+            }
+            set
+            {
+                this.bOTANICALField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string ZONE
+        {
+            get
+            {
+                return this.zONEField;
+            }
+            set
+            {
+                this.zONEField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string LIGHT
+        {
+            get
+            {
+                return this.lIGHTField;
+            }
+            set
+            {
+                this.lIGHTField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string PRICE
+        {
+            get
+            {
+                return this.pRICEField;
+            }
+            set
+            {
+                this.pRICEField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string AVAILABILITY
+        {
+            get
+            {
+                return this.aVAILABILITYField;
+            }
+            set
+            {
+                this.aVAILABILITYField = value;
+            }
+        }
+    }
+
+
+}
